d8716a639fc693eff2b6bf3ca225aad71f43b42c
Quentin <quentin.t@gmail.com>
2023-10-12 16:06:59 +0200

    Get ready for NPM

--- index.ts
+++
@@ -1,8 +0,0 @@
-import AppServer from './src/AppServer.ts'
-import MyTestApp from './MyTestApp.ts'
-
-const argv = require('minimist')(process.argv.slice(2))
-
-const appServer = new AppServer(() => new MyTestApp(), {
-  liveReload: argv['live-reload'],
-})
